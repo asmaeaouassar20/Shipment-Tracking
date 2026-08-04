56c41060aab4614d3f17a463ae6e81eade060e96
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-04 15:01:52 +0100

    add ShipmentStatus Enum

---
+++ src/main/java/com/algostyle/shipment/shipment/ShipmentStatus.java
@@ -0,0 +1,11 @@
+package com.algostyle.shipment.shipment;
+
+public enum ShipmentStatus {
+    ORDER_PLACES,
+    PROCESSING,
+    PICKED_UP,
+    IN_TRANSIT,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    EXCEPTION
+}

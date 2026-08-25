93af34233dfbf1d971f8df860717b49535e0e059
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-25 13:47:47 +0100

    add angular service

---
+++ frontend/src/app/services/shipment-service.ts
@@ -0,0 +1,10 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable,inject } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShipmentService {
+  private http = inject(HttpClient);
+}

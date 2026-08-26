c2aee7bd071dc92b680ec733c1bc74855693b292
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-08-26 13:18:03 +0100

    add CRUD methods in frontend service

--- frontend/src/app/services/shipment-service.ts
+++ frontend/src/app/services/shipment-service.ts
@@ -1,5 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import { Injectable,inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CreateShipmentRequest, Shipment, UpdateStatusRequest } from '../models/shipment.model';
 
 
 @Injectable({
@@ -7,4 +9,21 @@ import { Injectable,inject } from '@angular/core';
 })
 export class ShipmentService {
   private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/shipments';
+
+  createShipment(request : CreateShipmentRequest) : Observable<Shipment>{
+    return this.http.post<Shipment>(this.apiUrl , request);
+  }
+  getAllShipments() : Observable<Shipment[]>{
+    return this.http.get<Shipment[]>(this.apiUrl);
+  }
+  getShipmentById(id:number) : Observable<Shipment[]>{
+    return this.http.get<Shipment[]>(`${this.apiUrl}/${id}`);
+  }
+  getShipmentByTrackingNumber(trackingNumber:string) : Observable<Shipment>{
+    return this.http.get<Shipment>(`${this.apiUrl}/track/${trackingNumber}`);
+  }
+  updateShipment(id:number, request:UpdateStatusRequest) : Observable<Shipment>{
+    return this.http.patch<Shipment>(`${this.apiUrl}/${id}/status` , request);
+  }
 }

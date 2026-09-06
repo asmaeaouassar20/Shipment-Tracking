1e35070ab3b8aeac2556f0944226a9fb99b6cace
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-09-06 12:47:22 +0100

    fix bug : la mise à jour du dashboard et du select en temps réel

--- frontend/src/app/services/shipment-service.ts
+++ frontend/src/app/services/shipment-service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@angular/common/http';
-import { Injectable,inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable,inject, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { CreateShipmentRequest, Shipment, UpdateStatusRequest } from '../models/shipment.model';
 
 
@@ -11,12 +11,19 @@ export class ShipmentService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:8080/api/shipments';
 
+  shipments = signal<Shipment[]>([]); //liste de shipments partagée par toutel'application
+
   createShipment(request : CreateShipmentRequest) : Observable<Shipment>{
     return this.http.post<Shipment>(this.apiUrl , request);
   }
-  getAllShipments() : Observable<Shipment[]>{
-    return this.http.get<Shipment[]>(this.apiUrl);
-  }
+  getAllShipments(): Observable<Shipment[]> {
+  return this.http.get<Shipment[]>(this.apiUrl).pipe(
+    tap(shipments => {
+      this.shipments.set(shipments);
+    })
+  );
+}
+
   getShipmentById(id:number) : Observable<Shipment[]>{
     return this.http.get<Shipment[]>(`${this.apiUrl}/${id}`);
   }
